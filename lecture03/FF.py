85ae223797626fa170a81ab26eba691f8cb0be35
AlekseySimonov <90541836+AlekseySimonov@users.noreply.github.com>
2021-10-08 18:53:50 +0700

    Delete lecture03 directory

--- lecture03/FF.py
+++
@@ -1,9 +0,0 @@
-while True:
-    a = (int(input('Введите первое число: ')))
-    b = (int(input('Введите второе число: ')))
-    while a != b:
-        if a > b:
-            a = a - b
-        else:
-            b = b - a
-    print(a)
