11271828fe6e077363f1e43edd7e63e860b9b998
AlekseySimonov <90541836+AlekseySimonov@users.noreply.github.com>
2021-12-14 12:13:34 +0700

    Update 3.py

--- lecture04/3.py
+++ lecture04/3.py
@@ -1,19 +1,24 @@
 import random
-while True:
-    n = input('\n Введите количество случайных чисел: ')
-    if not n.isdigit():
-        print("Введите корректное число")
-    if n.isdigit():
-        n = int(n)
-        if n < 1:
-            print("Введите корректное число")
-        a = list(range(n+1))
-        a.pop(0)
-        random.shuffle(a)
-        print(a)
 
+n = input('Введите количество чисел: ')
 
-        random.shuffle(a)
-        while a != sorted(a):
-            random.shuffle(a)
-        print(a)
+if not n.isdigit():
+    print('Введите корректное число')
+
+if n.isdigit():
+    n = int(n)
+    nums = []
+    flag = True
+
+    for i in range(n):
+        nums.append(random.randrange(0, 100))
+
+    while flag:
+        random.shuffle(nums)
+        for i in range(n - 1):
+            if nums[i] < nums[i + 1]:
+                if i == n - 2:
+                    flag = False
+            else:
+                break
+        print(*nums)
